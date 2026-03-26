f8b7d245be941888dc88f9cd06844b18c86a1a00
Kevin Mota da Costa <costakevinn.ml@gmail.com>
2026-03-26 19:35:43 -0300

    limpar tudo

--- pages/dashboard.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-if not st.session_state.get("logged_in"):
-    st.warning("Você precisa fazer login primeiro.")
-    st.stop()
-
-st.title("Dashboard")
-st.info("Em breve.")
